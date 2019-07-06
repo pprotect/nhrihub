888385a4e234d15ba6ced3b92a9eb5cee029ec02
Les Nightingill <codehacker@comcast.net>
2019-07-05 18:03:53 -0700

    hopefully reduce occasional file download errors in test suite

--- spec/helpers/download_helpers.rb
+++ spec/helpers/download_helpers.rb
@@ -2,7 +2,7 @@ require 'rspec/core/shared_context'
 module DownloadHelpers
   extend RSpec::Core::SharedContext
   PATH = File.expand_path('../../../tmp/downloaded_files', __FILE__)
-  TIMEOUT = 10
+  TIMEOUT = 30 # for unknown reasons, it downloads sometimes pause midway, taking way too long
 
   def wait_for_download
     Timeout.timeout(TIMEOUT) do
@@ -32,6 +32,10 @@ module DownloadHelpers
 
   def clear_downloaded_files
     FileUtils.rm_f(downloaded_files)
+  end
+
+  before do
+    clear_downloaded_files
   end
 
   after do
