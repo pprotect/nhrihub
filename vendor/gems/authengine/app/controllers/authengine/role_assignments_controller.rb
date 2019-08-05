5f39aca7254d778ff334f6802897c22845d7dcf4
Les Nightingill <codehacker@comcast.net>
2019-08-05 06:47:14 -0700

    tweaks to formatting of role assignments log

--- vendor/gems/authengine/app/controllers/authengine/role_assignments_controller.rb
+++ vendor/gems/authengine/app/controllers/authengine/role_assignments_controller.rb
@@ -1,5 +1,5 @@
 class Authengine::RoleAssignmentsController < ApplicationController
   def index
-    @role_assignments = RoleAssignment.all
+    @role_assignments = RoleAssignment.order("created_at desc").all
   end
 end
