aa816c75cd52f134056ea8d463e6e210375e5e65
Les Nightingill <codehacker@comcast.net>
2019-12-05 15:00:39 -0800

    fix for app database bootstrap problem on new server

--- vendor/gems/authengine/app/models/role_administration_logger.rb
+++ vendor/gems/authengine/app/models/role_administration_logger.rb
@@ -1,8 +1,10 @@
 class RoleAdministrationLogger
 
   def self.after_save(role)
-    RoleAssignment.create(assigner_id: role.administrator&.id, action: 'create', assignee_id: nil, role_name: role.name)
-  rescue PG::UndefinedTable
+      RoleAssignment.create(assigner_id: role.administrator&.id, action: 'create', assignee_id: nil, role_name: role.name)
+  rescue ActiveRecord::StatementInvalid
+    # rescue from role_assignments postgres error relation does not exist
+    # when bootstrapping a new app, the role_assignments table may not exist
   end
 
   def self.after_destroy(role)
