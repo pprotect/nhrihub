5deca39ac41e69fc3979f5736b751b1cba35abe7
Les Nightingill <codehacker@comcast.net>
2020-03-05 08:32:56 -0800

    retrospectively insert migration that has been lost

---
+++ vendor/gems/complaints/db/migrate/20200203191804_create_offices_office_groups.rb
@@ -0,0 +1,18 @@
+class CreateOfficesOfficeGroups < ActiveRecord::Migration[6.0]
+  def change
+    create_table "office_groups", force: :cascade do |t|
+      t.string "name"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+    end
+
+    create_table "offices", force: :cascade do |t|
+      t.integer "office_group_id"
+      t.string "name"
+      t.string "short_name"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.integer "province_id"
+    end
+  end
+end
