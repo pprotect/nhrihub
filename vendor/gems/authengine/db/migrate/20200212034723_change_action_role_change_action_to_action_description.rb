dfe9d50fed713f807ae04d9fa7bf1ccb6d7a5bfc
Les Nightingill <codehacker@comcast.net>
2020-04-01 12:56:22 -0700

    hacking migrations

--- vendor/gems/authengine/db/migrate/20200212034723_change_action_role_change_action_to_action_description.rb
+++ vendor/gems/authengine/db/migrate/20200212034723_change_action_role_change_action_to_action_description.rb
@@ -1,6 +1,6 @@
 class ChangeActionRoleChangeActionToActionDescription < ActiveRecord::Migration[6.0]
   def change
-    remove_column :action_role_changes, :action
+    #remove_column :action_role_changes, :action
     add_column :action_role_changes, :action_description, :string
   end
 end
